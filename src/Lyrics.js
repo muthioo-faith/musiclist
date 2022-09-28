b2b97405a22943a3c20e2f387ef029ca55580773
muthioo-faith <muthioofaith@gmail.com>
2022-09-28 11:14:14 +0300

    Lyrics

--- src/Lyrics.js
+++ src/Lyrics.js
@@ -1,66 +1,42 @@
-
-// import './App.css';
-import Axios from 'axios';
+// import Axios from 'axios';
 import React, { useState, useEffect } from 'react';
-  
-function Lyrics() {
-    // const [artist, setArtist] = useState("");
-    const [song, setSong] = useState([]);
-    // const [lyrics, setLyrics] = useState("");
-  
-    // function searchLyrics() {
-    //     if (artist === "" || song === "") {
-    //         return;
-    //     }
 
-    const getSongs =async ()=>{
-        await fetch("https://api.lyrics.ovh")
+function Lyrics() {
+    const [product, setProduct] = useState([]);
+
+    const getProducts = async () => {
+        await fetch("https://dummyjson.com/products")
         .then((response)=> response.json())
-        .then((song)=> setSong(song))
+        .then((product)=> {
+         setProduct(product.products) 
+            console.log(product.products)
+        })
         .catch((error)=>{
             throw new Error(error)
         }) 
 
-        }
+     }
+
         useEffect(()=>{
-            getSongs()
-        }, [])
-
-//         Axios.get(
-// ${artist}/${song}`).then(res => {
-//             console.log(res.data.lyrics);
-//             setLyrics(res.data.lyrics);
-//         })
-//     }
-  
-    return (
-        <div className="Main">
-            <h1>Lyrics Finder</h1>
+            getProducts()
+        }, []);
+        
+return (
+        <div className="Main" >
+            <h1>products</h1>
             {
-                song.map((song)=>
-                <div key={song.name}>
-                    <h2>{song.username}</h2>
-                    <p>{song.city}</p>
-                    <p>{song.country}</p>
-                    <img src={song.thumbnail} alt = "image"/>
+                product.map((product)=>
+                <div key = {product.id}>
+                    <h2>{product.title}</h2>
+                    <p>{product.price}</p>
+                    <p>{product.discountPercentage}</p>
+                    <p>{product.category}</p>
+                    <img src={product.thumbnail} alt = "image"/>
 
-                </div>)
-
-            }
-  
-            {/* <input className="inp" type="text" 
-                placeholder='Artist name'
-                onChange={(e) => { setArtist(e.target.value) }} />
-            <input className="inp" type="text" 
-                placeholder='Song name'
-                onChange={(e) => { setSong(e.target.value) }} />
-            <button className="btn" 
-                onClick={() => searchLyrics()}>
-                    Search button</button>
-            <hr />
-            <pre>{lyrics}</pre> */}
+                </div>
+                )}            
         </div>
     );
 }
- 
+
 export default Lyrics;
